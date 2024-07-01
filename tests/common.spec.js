da3e4f14244a4450aa851bd5cb2905b597860429
Rafail Ferre <154566757+RafailFerre@users.noreply.github.com>
2024-07-01 15:33:18 -0400

    created common.spec, modified playwright config (#5)

---
+++ tests/common.spec.js
@@ -0,0 +1,32 @@
+import { test, expect } from '@playwright/test';
+import { before, beforeEach } from 'node:test';
+
+test.describe('COMMON', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('/user/login')
+
+        await page.locator('#normal_login_email').fill('raf-test@gmail.com')
+        await page.locator('#normal_login_password').fill('12345')
+        await page.locator('button[type="submit"]').click()
+    })
+    test('Navigation - Courses', async ({ page }) => {
+        await page.getByTestId('topmenu-Courses').click()
+        await expect(page).toHaveURL('/course')
+        await expect(page.getByText('Interactive Courses')).toBeVisible()
+    });
+    test('Navigation - Challenges', async ({ page }) => {
+        await page.getByTestId('topmenu-Challenges').click()
+        await expect(page).toHaveURL('/challenge?limit=30&page=1')
+        await expect(page.getByText('Coding challenges')).toBeVisible()
+    });
+    test('Navigation - Interview Questions', async ({ page }) => {
+        await page.getByTestId('topmenu-Interview Questions').click()
+        await expect(page).toHaveURL('/flash')
+        await expect(page.getByText('Interview practice cards')).toBeVisible()
+    });
+    test('Navigation - Diary', async ({ page }) => {
+        await page.getByTestId('topmenu-Diary').click()
+        await expect(page).toHaveURL('/diary?page=1')
+        await expect(page.getByText('Diary of progress')).toBeVisible()
+    });
+});
